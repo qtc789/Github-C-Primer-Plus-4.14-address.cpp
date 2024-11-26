6286f8954f1711fd590a3573c954ef5f32e20e1a
qtc789 <qiantongci@126.com>
2024-11-26 21:38:30 +0800

    Add files via upload

---
+++ 4.14/4.14 address.cpp/4.14 address.cpp/4.14 address.cpp.cpp
@@ -0,0 +1,20 @@
+﻿// 4.14 address.cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+
+{
+    using namespace std;
+    int donuts = 6;
+    double cups = 4.5;
+    cout << "dounts value=   " << donuts;
+    cout << "and dounuts address=   " << &donuts << endl;
+    cout << "cups value=   " << cups;
+    cout << "and cups address=   " << &cups << endl;
+
+    
+}
+
+
